3f28b1d0c6882285b378f4953efb6646cb16ae98
Wilma Hammarshall <89970576+Hammarshall@users.noreply.github.com>
2023-01-22 18:36:39 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Webbsite
+This project will develop to be some kind of portfolio. OBS! Contactform is not activated yet.
---
+++ bootstrap/random.js
@@ -0,0 +1,9 @@
+<div>
+  <a target="_blank" href="https://icons8.com/icon/20405/chef-hat">
+    Chef Hat
+  </a>{" "}
+  icon by{" "}
+  <a target="_blank" href="https://icons8.com">
+    Icons8
+  </a>
+</div>;
